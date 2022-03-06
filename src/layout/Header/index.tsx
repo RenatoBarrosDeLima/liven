eacba7e57b88e4789ec7f1b3c77d15327a690ef8
unknown <renato.dev.mobi@gmail.com>
2022-03-06 15:07:44 -0300

    feat: add styled-components

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import { Container } from "./styles";
+
 const Header = () => {
   return (
-    <div>
+    <Container>
       <h1> Aqui é o HEADER</h1>
-    </div>
+    </Container>
   );
 };
 
