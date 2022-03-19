60972966b47f3a6eba8e2f736bd363b95265c4ee
unknown <renato.dev.mobi@gmail.com>
2022-03-19 14:28:13 -0300

    feat: add e.preventDefault form search

--- src/Components/Search/index.tsx
+++ src/Components/Search/index.tsx
@@ -3,10 +3,14 @@ import React from "react";
 import { ButtonSearch, Container, Form, InputSearch } from "./styles";
 
 const Search = () => {
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <Container>
-        <Form>
+        <Form onSubmit={(e) => handleSearch(e)}>
           <InputSearch placeholder="Faça sua pesquisa aqui" />
           <ButtonSearch>Buscar</ButtonSearch>
         </Form>
