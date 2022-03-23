d209bec5d36b22cb4c8470baa9e617cb8f27a063
unknown <renato.dev.mobi@gmail.com>
2022-03-23 11:21:48 -0300

    feat: add comments index e services api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
+// Importação do componente Home
 import Home from "../containers/Home";
+
+// Importação da instância da API
 import api from "../services/api";
 
 const Page = ({ products, menus }: any) => {
+  // Chama o componente já passando os produtos e as categorias da API fakestoreapi
   return <Home products={products} menus={menus} />;
 };
 
+// Uso do Server-side rendering (SSR), já faz as requisições dos produtos e dos menus no lado do servidor
 export const getServerSideProps = async () => {
   try {
+    // Faz a consulta dos produtos na API fakestoreapi e atribui em uma constante chamada products
     const { data: products } = await api.get("products");
+
+    // Faz a consulta das categorias na API fakestoreapi e atribui em uma constante chamada menus
     const { data: menus } = await api.get("products/categories");
 
     return { props: { products, menus } };
   } catch (e) {
+    // Caso dê algum problema a API os produtos e os menus irão ser vazios
     return { props: { products: [], menus: [] } };
   }
 };
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
+// Importação do axios, responsável por fazer as chamadas na API
 import axios from "axios";
 
+// Cria uma instância no axios que será utilizado por toda a aplicação usando a URL da fakestoreapi
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
