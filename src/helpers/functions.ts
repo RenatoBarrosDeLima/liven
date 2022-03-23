8c948b73f4a36ff621bf6d5c28f20b489179b412
unknown <renato.dev.mobi@gmail.com>
2022-03-23 11:25:05 -0300

    feat: add comments functions helpers

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,3 +1,4 @@
+// Função que converte um número em moeda Real brasileiro
 export const moneyFormat = (value = 0) => {
   return new Intl.NumberFormat("pt-BR", {
     style: "currency",
@@ -5,14 +6,17 @@ export const moneyFormat = (value = 0) => {
   }).format(value);
 };
 
+// Função que faz o somatório dos preços de uma lista, levando em consideração a quantidade de items da lista
 export const sum = (items: any) => {
   return items.reduce(getTotal, 0);
 };
 
+// Função que verifica se um número é fracionário ou não
 export const fractional = (rate = 0) => {
   return rate % 2 != 0;
 };
 
+// Função que multiplica o preço pela quantidade e guarda o total
 function getTotal(total: number, item: any) {
   return total + item.price * item.quantity;
 }
